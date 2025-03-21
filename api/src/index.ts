1b6cff89e7bae776326895675e936fba694009ae
Carlos-Cerqueira <carlosleite.884@gmail.com>
2025-03-21 15:05:47 -0300

    first commit

--- api/src/index.ts
+++ api/src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 export const io = new Server(server);
 
-mongoose.connect('mongodb+srv://admin:root@cluster0.zkvuqyj.mongodb.net/')
+mongoose.connect("") // Insira a URI do Mongo.DB aqui
 	.then(() => {
 		const port = 3001;
 
